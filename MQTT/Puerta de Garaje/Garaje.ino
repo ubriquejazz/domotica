cffeecee574600dc0adf875617017b0a00bbcc0f
Marcelo Suisse <ubriquejazz@gmail.com>
2024-05-22 15:48:42 +0200

    Create a config file
    Parameters have been removed. Better than to have them embedded in the code

--- MQTT/Puerta de Garaje/Garaje.ino
+++ MQTT/Puerta de Garaje/Garaje.ino
@@ -24,16 +24,7 @@ Configuración que debes poner en el archivo mqtt.yaml
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoOTA.h>
-
-const char* ssid  = "SSID"; // Nombre de nuestra red Wifi
-const char* password = "CONTRASEÑA"; // Contraseña de nuestra ede Wifi
-const char* mqtt_server = "XXX.XXX.X.XX"; // I.P. de nuestro servidor MQTT (Home Assistant)
-int mqttport = 1883;
-const char* mqttusuario = "MQTT"; // Nombre de usuario MQTT en Home Assistant
-const char* mqttpass = "XXX"; // Contraseña para el usuario de MQTT
-#define CLIENT_ID   "Puerta_Garaje"  // Nombre que le queramos dar al dispositivo                 
-#define MQTT_TOPIC  "garaje"         // Topic raíz
-#define LUZ   "garaje/luces/comando" // Topic para encender la luz del garaje si la tenemos domotizada
+#include "config.h"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
