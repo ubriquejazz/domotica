5b846afb3b70286305c4117b62215b7a5e21950c
Marcelo Suisse <ubriquejazz@gmail.com>
2024-05-29 13:07:07 +0200

    Name convention

--- MQTT/data/config_garaje.h
+++ MQTT/data/config_garaje.h
@@ -4,6 +4,7 @@ const char* mqtt_server = "192.168.1.98";   // I.P. de nuestro servidor MQTT (Ho
 int mqttport = 1883;
 const char* mqttusuario = "MQTT";       // Nombre de usuario MQTT en Home Assistant
 const char* mqttpass = "XXX";           // Contraseña para el usuario de MQTT
+
 #define CLIENT_ID   "Puerta_Garaje"     // Nombre que le queramos dar al dispositivo                 
 #define MQTT_TOPIC  "garaje"            // Topic raíz
 #define LUZ   "garaje/luces/comando"    // Topic para encender la luz del garaje si la tenemos domotizada
