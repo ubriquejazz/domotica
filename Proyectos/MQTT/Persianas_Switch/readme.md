0bffda76ca9e05adf7759a2fcb5499a71e5aa830
Marcelo Suisse <ubriquejazz@gmail.com>
2025-01-09 18:49:17 +0100

    No mas MQTT

--- Proyectos/MQTT/Persianas_Switch/readme.md
+++
@@ -1,4 +0,0 @@
-# Pulsadores
-
-Añadida funconalidad para subir y bajar las persianas mediante los pulsadores 
-incluso cuando no tengamos conexión a Wifi o MQTT.
--- Proyectos/MQTT/data/config_sala.h
+++
@@ -1,8 +0,0 @@
-const char* ssid = "SSID";                  // Nombre de tu SSID
-const char* password = "PASSWORD";          // Contraseña de tu SSID
-const char* mqtt_server = "XXX.XXX.X.XX";   // IP de tu servidor MQTT
-const char* mqttusuario = "MQTT";           // Usuario MQTT en Home Assistant
-const char* mqttpass = "PASS_MQTT";         // Contraseña para el usuario MQTT 
-
-#define CLIENT_ID   "Persiana_Sala"   // ID único en tu sistema
-#define MQTT_TOPIC  "persianas/sala"  // mismo que tengas en cover.yaml
